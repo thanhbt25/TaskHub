c24f86431ced85cd0afda62b6ccaede477556e14
Thanh Bui <tbui9887@gmail.com>
2026-07-28 15:51:59 +0700

    feat: setup project structure, fastapi, sqlite, docker and alembic config

---
+++ app/core/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "sqlite:///./database.sqlite"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
---
+++ app/repositories/item_repo.py
@@ -0,0 +1,15 @@
+from schemas.item_schema import ItemCreate, ItemResponse
+
+# Mock database tạm thời
+FAKE_DB = []
+CURRENT_ID = 1
+
+def get_all_items() -> list[ItemResponse]:
+    return FAKE_DB
+
+def create_item(item: ItemCreate) -> ItemResponse:
+    global CURRENT_ID
+    new_item = ItemResponse(id=CURRENT_ID, **item.model_dump())
+    FAKE_DB.append(new_item)
+    CURRENT_ID += 1
+    return new_item
